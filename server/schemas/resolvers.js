60d2d0cf9fa0c6903bd9dd23c829d235e3ebbe5b
Maddy <madisonschaaf48@gmail.com>
2022-06-09 17:33:57 -0600

    updated queries and mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,1 +1,22 @@
+const {AuthenticationError} = require('apollo-server-express')
+
 //pull from controllers
+const resolvers = {
+Query: {
+    // 
+},
+
+
+
+Mutation: {
+    addUser: async (parent, args) => {
+        const user = await User.create(args)
+        return user;
+
+    },
+    login: async () => {
+
+    }
+}
+
+}
